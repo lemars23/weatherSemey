e2566e07e83e344be3b0eab3dc8a6d1725d111fa
Alisher <alishersmagylov@gmail.com>
2023-10-04 11:53:56 +0600

    Add and change script

--- js/main.js
+++ js/main.js
@@ -1,24 +1,81 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-    const lastUpdate = document.querySelector(".weather_update_time");
-    const currentTemp = document.querySelector(".current_temp");
-    const currentTempFeelsLike = document.querySelector(".current_temp_feelslike");
-    const currentCondition = document.querySelector(".current_condition");
-    const todayTempMin = document.querySelector(".today_temp_min");
-    const todayTempMax = document.querySelector(".today_temp_max");
-    fetch("http://api.weatherapi.com/v1/forecast.json?key=c0bf2838ecf5423e9be101133232809&q=Semey&days=7&aqi=no&alerts=no&lang=ru").then(response => response.json()).then(json => {
-        const apiLastUpdate = json["current"]["last_updated"];
-        const apiCurrentTemp = json["current"]["temp_c"];
-        const apiCurrentTempFeelsLike = json["current"]["feelslike_c"];
-        const apiCurrentCondition = json["current"]["condition"]["text"];
-        const apiTodayMinTemp = json["forecast"]["forecastday"][0]["day"]["mintemp_c"];
-        const apiTodayMaxTemp = json["forecast"]["forecastday"][0]["day"]["maxtemp_c"];
-        lastUpdate.innerHTML = apiLastUpdate;
-        currentTemp.innerHTML = apiCurrentTemp;
-        currentTempFeelsLike.innerHTML = apiCurrentTempFeelsLike;
-        currentCondition.innerHTML = apiCurrentCondition;
-        todayTempMin.innerHTML = apiTodayMinTemp;
-        todayTempMax.innerHTML = apiTodayMaxTemp;
-        console.log(apiCurrentTempFeelsLike);
+    const currentTemp = document.querySelector(".weather_now_temp_number");
+    const currentState = document.querySelector(".weather_now_state");
+    const currentStateImage = document.querySelector(".weather_now_image");
+    const currentDate = document.querySelector(".weather_now_date");
+    const weatherWeek = document.querySelector(".weather_week");
+    const todayMaxTemp = document.querySelector(".weather_today_max");
+    const todayMinTemp = document.querySelector(".weather_today_min");
+    
+    const todayDate = {
+        date: new Date(),
+        week: [
+            "Воскресенье",
+            "Понедельник",
+            "Вторник",
+            "Среда",
+            "Четверг",
+            "Пятница",
+            "Суббота",
+        ],
+        nowYear: function() {
+            return this.date.getFullYear();
+        },
+        nowMonth: function() {
+            return this.date.toLocaleString("default", {month: "long"}).charAt(0).toUpperCase() + this.date.toLocaleString("default", {month: "long"}).slice(1);
+        },
+        nowDay: function() {
+            return this.date.getDate();
+        },
+        getDayOfWeek: function(date) {
+            return this.week[date];
+        }
+    };
+
+    currentDate.innerHTML = `${todayDate.nowMonth()} ${todayDate.nowDay()}, ${todayDate.nowYear()}`;
+    
+    fetch("http://api.weatherapi.com/v1/forecast.json?key=c0bf2838ecf5423e9be101133232809&q=Semey&days=8&aqi=no&alerts=no&lang=ru").then(response => response.json()).then(json => {
+        currentTemp.innerHTML = Math.floor(json.current.temp_c);
+        currentState.innerHTML = json.current.condition.text;
+
+        if(json.current.condition.text === "Местами дождь") {
+            currentStateImage.src = "../images/rain.png";
+        } else if(json.current.condition.text === "Солнечно") {
+            currentStateImage.src = "../images/clear.png";
+        } else if(json.current.condition.text === "Переменная облачность") {
+            currentStateImage.src = "../images/cloudy.png";
+        }
+
+        for(let i = 0; i < 7; i++) {
+            const weatherDay = document.createElement("li");
+            weatherDay.classList.add("weather_day");
+            weatherDay.innerHTML = `
+                                        <div class="weather_day_name">${todayDate.getDayOfWeek(new Date(json.forecast.forecastday[i].date).getDay())}</div>
+                                        <div class="weather_day_picture">
+                                            <img
+                                                src="${setImage(json.forecast.forecastday[i].day.condition.text)}"
+                                                alt="weather_day_image"
+                                                class="weather_day_image"
+                                            />
+                                        </div>
+                                        <div class="weather_day_noon">${Math.floor(json.forecast.forecastday[i].day.maxtemp_c)}&deg;C</div>
+                                        <div class="weather_day_night">${Math.floor(json.forecast.forecastday[i].day.mintemp_c)}&deg;C</div>
+            `;
+            weatherWeek.append(weatherDay);
+        }
     });
+
+    // console.log((new Date()).getDay());
+
+
+    function setImage(text) {
+        if(text === "Местами дождь") {
+            return "../images/rain.png";
+        } else if(text === "Солнечно") {
+            return "../images/clear.png";
+        } else if(text === "Переменная облачность") {
+            return "../images/cloudy.png";
+        }
+    }
 });
